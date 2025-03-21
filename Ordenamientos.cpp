27ca0d80005961907211af350c5ba05a288793cb
tamayo2 <juan.tamayo3@utp.edu.co>
2025-03-21 00:48:56 -0500

    Funciones para utilizar

--- Ordenamientos.cpp
+++ Ordenamientos.cpp
@@ -6,3 +6,11 @@
 using namespace std;
 
 vector<int> numeros;
+
+void leerArchivo(string nombreArchivo);
+void selectionSort();
+void heapSort();
+void countingSort();
+void quickSort(int izquierda, int derecha);
+bool estaOrdenado();
+void mostrarMenu();
